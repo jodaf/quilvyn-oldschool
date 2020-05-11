ee061f9208b632315260be840f9920e417bdc9f7
Jim Hayes <spinaltap12345@gmail.com>
2020-05-11 15:27:38 -0700

    Bump to v1.8.

--- FirstEdition.js
+++ FirstEdition.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var FirstEdition_VERSION = '1.7.1.1';
+var FirstEdition_VERSION = '1.8.1.0';
 
 /*
  * This module loads the rules from the 1st edition core rules.  The FirstEdition
